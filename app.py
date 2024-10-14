ae5821bd8f64505d74a23d06a1f782117f2abe9c
observer04 <tripathyomm09@gmail.com>
2024-10-15 01:23:43 +0530

    The web app is ready. Hooray!!

--- app.py
+++ app.py
@@ -14,15 +14,24 @@ def home():
     return render_template('home.html')
 
 #following function will be called when the user hits the predict button
+# @app.route('/predict', methods=['POST'])
+# def predict_api():
+#    data= request.get_json()['data']
+#    print(data)
+#    print(np.array(list(data.values())).reshape(1,-1))
+#    new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
+#    output= regmodel.predict(new_data)
+#    print(output[0])
+#    return jsonify(output[0])
+
 @app.route('/predict', methods=['POST'])
-def predict_api():
-   data= request.get_json()['data']
-   print(data)
-   print(np.array(list(data.values())).reshape(1,-1))
-   new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
-   output= regmodel.predict(new_data)
-   print(output[0])
-   return jsonify(output[0])
+def predict():
+   data= [float(x) for x in request.form.values()]
+   final_input= np.array(data).reshape(1,-1)
+   print(final_input)
+   output= regmodel.predict(final_input)[0]
+   return render_template('home.html', prediction_text='Predicted House Price: {}'.format(output))
+   
 
 if __name__ == '__main__':
    app.run(debug=True)
